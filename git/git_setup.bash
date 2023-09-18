af1353519d5c7f92d24865f6bd65ec6bd7fe32ca
sifiso <simabuz022@student.wethinkcode.co.za>
2023-09-18 10:38:56 +0200

    git global email complete

--- git/git_setup.bash
+++ git/git_setup.bash
@@ -11,6 +11,18 @@ while true; do
     fi
 done
 
+#change git global username
 git config --global user.name "$git_username"
 
+git_email=""
+while true; do
+    read -p "Plese enter your new git global email: " user_input_2
+    read -p "Following git global email correct: $user_input_1? (y/n): " correct_email
+    if [ "$correct_email" = "y" ]; then
+        git_email="$user_input_2"
+        break
+    fi
+done
 
+#change git global email
+git config --global user.email "$git_email"
